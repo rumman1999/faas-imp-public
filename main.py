0c9cb8915988c76a518236f2cd9467ee88b4b5c7
rummangaian <mohammad.r@mobiusdtaas.ai>
2025-08-16 14:18:37 +0530

    added the setup config

---
+++ main.py
@@ -0,0 +1,7 @@
+from mycalc import add, power, sqrt, factorial, logarithm
+
+print("Add:", add(10, 5))          # 15
+print("Power:", power(2, 8))       # 256
+print("Sqrt:", sqrt(49))           # 7.0
+print("Factorial:", factorial(5))  # 120
+print("Log:", logarithm(100, 10))  # 2.0
--- test/test.py
+++ test/test.py
@@ -1,4 +1,5 @@
 from mycalc import add, divide
+import pytest
 
 def test_add():
     assert add(2, 3) == 5
